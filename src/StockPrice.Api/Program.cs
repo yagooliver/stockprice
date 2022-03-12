0f097a7827980e11e364e5f34346f75e86c494b8
Yago Oliveira <yago.oliveira.ce@protonmail.com>
2022-03-12 17:04:46 -0300

    ci.yaml file added

--- src/StockPrice.Api/Program.cs
+++ src/StockPrice.Api/Program.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+using Microsoft.OpenApi.Models;
 using StockPrice.Api;
 using StockPrice.WebServices.Interface;
 
@@ -11,15 +13,27 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.RegisterServices(builder.Configuration);
 
+builder.Services.AddSwaggerGen(options => 
+{
+    options.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Version = "v1",
+        Title = "Stock Price"
+    });
+    foreach(var xmlFile in Directory.GetFiles(AppContext.BaseDirectory, "*.xml", SearchOption.AllDirectories))
+    {
+        options.IncludeXmlComments(xmlFile);
+    }
+
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+    
+app.UseSwaggerUI();
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 
 app.MapGet("/stock", async (string stock, IStockCallerService service) => await service.GetStock(stock));
